82f412d05e18cff6e4aaaf6e2cbfb88cbe5e32d3
jtmilleer <jtmilleer05@gmail.com>
2026-02-09 23:10:30 -0600

    refactor

--- Homework_3/problem2.v
+++ Homework_3/problem2.v
@@ -1,7 +1,5 @@
 module problem_2 (output y, input a, b, c);
     always @(a or b or c) begin
-        if (a == 1) begin
-            y = 1;
-        end else y = c;
+        y = a | c;
     end
 endmodule
